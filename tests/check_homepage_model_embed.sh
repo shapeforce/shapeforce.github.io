960c8389e2415486f58df1cdc2dab48ec4f7e434
Xandert <xandert@Xanderts-Mbp.local>
2026-03-21 12:13:21 +0800

    style: widen 3d model gallery layout

--- tests/check_homepage_model_embed.sh
+++ tests/check_homepage_model_embed.sh
@@ -9,6 +9,7 @@ model_line=$(rg -n '<h2 class="title is-3 has-text-centered">3D Model</h2>' "$fi
 bibtex_line=$(rg -n '<section class="section" id="BibTeX">' "$file" | cut -d: -f1)
 
 rg -q '<h2 class="title is-3 has-text-centered">3D Model</h2>' "$file"
+rg -q 'class="model-showcase-shell"' "$file"
 rg -q 'Explore the printable parts in 3D on Sketchfab\.' "$file"
 rg -q 'TPU Core' "$file"
 rg -q 'PLA Support' "$file"
